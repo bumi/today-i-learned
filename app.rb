065aaed775d1b2234858c8ad90984091640528e8
Michael Bumann <michael@railslove.com>
2014-10-06 16:20:15 +0200

    add brain

--- app.rb
+++ app.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
 require 'faraday'
-
+require 'yaml'
 $faraday = Faraday.new(:url => 'https://api.justyo.co/') do |faraday|
   faraday.request  :url_encoded             # form-encode POST params
   faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
 end
 
+$brain = YAML.load(File.read('./brain.yml'))
 get '/' do
   'Subscribe to TODAYILEARNED on YO! to receive a daily little piece of knowledge'
+end
+
+get '/today' do
+  $brain.sample
 end
 
 get '/gimme' do
